4f760c0344b913abe2ee17ab5bf8a4f54f9613cf
t-lamp11 <t-lamp11@users.noreply.github.com>
2019-07-16 15:08:25 -0500

    Third Try

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var HTTPS = require('https'),
 
 function respond() {
     var request = JSON.parse(this.req.chunks[0]),
-        botRegex = /cat\s?fact[sz]?/i;
+        botRegex = /cat\s*facts*/i;
 
     if(request.text && botRegex.test(request.text)) {
         this.res.writeHead(200);
