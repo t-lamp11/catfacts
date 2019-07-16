b12fbc37b2c2027236de3cd44e65419240557aeb
t-lamp11 <t-lamp11@users.noreply.github.com>
2019-07-16 15:03:01 -0500

    Actually Fixed Catfacts regex

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var HTTPS = require('https'),
 
 function respond() {
     var request = JSON.parse(this.req.chunks[0]),
-        botRegex = /cat?fact?/i;
+        botRegex = /cat\s?fact[sz]?/i;
 
     if(request.text && botRegex.test(request.text)) {
         this.res.writeHead(200);
